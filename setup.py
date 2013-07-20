162fef70c099158ec43345b9c2a95eb7c66658d1
Saul Shanabrook <s.shanabrook@gmail.com>
2013-07-20 11:14:27 -0400

    v0.1.1
    * Added safety check for invalidation middleware, so that it will pass on project tests, where sometimes the caching isn't running as it would on a normal request-response.
    * Added recommendation to use `django-response-timeout` for setting expires headers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-dumper',
-    version='0.1.0',
+    version='0.1.1',
     author='Saul Shanabrook',
     author_email='s.shanabrook@gmail.com',
     packages=find_packages(),
