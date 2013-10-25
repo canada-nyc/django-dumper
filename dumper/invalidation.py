bca6f6041e9f49d1d25d7a9c4cb88080d88c45b1
Saul Shanabrook <s.shanabrook@gmail.com>
2013-10-24 20:12:56 -0400

    Comment concerning differences in keys per path

--- dumper/invalidation.py
+++ dumper/invalidation.py
@@ -11,4 +11,11 @@ def invalidate_paths(paths):
 
 
 def all_cache_keys_from_path(path):
+    '''
+    Each path can actually have multiple cached entries, varying based on different HTTP
+    methods. So a GET request will have a different cached response from a HEAD request.
+    
+    In order to invalidate a path, we must first know all the different cache keys that the
+    path might have been cached at. This returns those keys
+    '''
     return [dumper.utils.cache_key(path, method) for method in dumper.settings.CACHABLE_METHODS]
