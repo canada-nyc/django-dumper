224423f3fa3dd826e8f24ab629b03b21233e974e
Saul <s.shanabrook@gmail.com>
2013-10-26 14:30:33 -0400

    Bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-dumper',
-    version='0.2.0',
+    version='0.2.1',
     author='Saul Shanabrook',
     author_email='s.shanabrook@gmail.com',
     packages=find_packages(),
