f61cc252198d8eb38c8af3c1b89ff6e16d1d5fe3
Saul Shanabrook <s.shanabrook@gmail.com>
2015-12-16 09:08:38 -0500

    define templates for django 1.10

--- dumper/settings.py
+++ dumper/settings.py
@@ -5,6 +5,7 @@ CACHE_ALIAS = getattr(settings, 'DUMPER_CACHE_ALIAS', 'default')
 KEY_PREFIX = getattr(settings, 'DUMPER_KEY_PREFIX', 'dumper.cached_path.')
 CACHABLE_METHODS = ['HEAD', 'GET']
 CACHABLE_RESPONSE_CODES = [200]
+TEMPLATES = []
 
 
 def PATH_IGNORE_REGEX():
